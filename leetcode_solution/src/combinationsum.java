0bf0730063572d40d596cb62a18d5f263fdb7569
steve <panj3457@myumanitoba.ca>
2018-10-10 21:20:59 -0500

    Beat 75.11% applying dfs&&pruning

--- leetcode_solution/src/combinationsum.java
+++ leetcode_solution/src/combinationsum.java
@@ -1,72 +1,35 @@
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 public class combinationsum {
 	public combinationsum(){
 		
 	}
-	
+	///pruning & depth first search, beat 75.11%
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
-		List<List<Integer>> result = new LinkedList<List<Integer>>();
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		Arrays.sort(candidates);
-		List<Integer> temp=	new LinkedList<Integer>();
+		List<Integer> temp=	new ArrayList<Integer>();
 		combinationSumHelper(candidates,target,temp,0,result,0);
 		return result;
     }
 	
 	public void combinationSumHelper(int[] candidates, int target, List<Integer> candidate, int sum, List<List<Integer>> result,int index) {
-		if(sum>target){
-			return;
-		}else if(sum==target){
-			result.add(candidate);
-			return;
-		}
-		
+		///stop branching if one combination is found or the current branch exceeds the target value
+		///branch start from the given index to avoid duplicate trials 
 		for(int i=index;i<candidates.length;i++){
-			
-			try {
-				List<Integer> temp = deepCopy(candidate);
+			if(sum+candidates[i]<target){
+				List<Integer> temp = new ArrayList<Integer>();
+				temp.addAll(candidate);
 				temp.add(candidates[i]);
 				combinationSumHelper(candidates, target, temp, sum+candidates[i], result,i);
-				
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			}else if(sum+candidates[i]==target){
+				candidate.add(candidates[i]);
+				result.add(candidate);
+				return;
+			}else{
+				return;
 			}
 		}
-		
-		
-		
 	}
-	
-	
-	public boolean test(List<List<Integer>> l){
-		for(List<Integer> list: l){
-			for(Integer i:list){
-				System.out.print(i+" ");
-			}
-			System.out.print("\n");
-		}
-		return false;
-	}
-	
-	public <T> List<T> deepCopy(List<T> src) throws IOException, ClassNotFoundException {  
-	    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();  
-	    ObjectOutputStream out = new ObjectOutputStream(byteOut);  
-	    out.writeObject(src);  
-	  
-	    ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());  
-	    ObjectInputStream in = new ObjectInputStream(byteIn);  
-	    @SuppressWarnings("unchecked")  
-	    List<T> dest = (List<T>) in.readObject();  
-	    return dest;  
-	}  
 }
--- leetcode_solution/src/solution.java
+++ leetcode_solution/src/solution.java
@@ -5,9 +5,9 @@ public class solution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		combinationsum s = new combinationsum();
-		int[] i = {2,3,6,7};
-		List<List<Integer>> l = s.combinationSum(i, 7);
-		System.out.println(s.test(l));
+		int[] i = {2,3,5};
+		List<List<Integer>> l = s.combinationSum(i, 8);
+		
 	}
 	
 }
